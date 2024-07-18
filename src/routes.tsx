0a5e3696f7b2756ecab59914c087ed34c9a5c822
Vinícius Cardoso Junqueira <vinikrdoso@hotmail.com>
2024-07-18 12:31:59 +0100

    feat: create orders component

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
+import { Orders } from './pages/app/orders'
 import { SignIn } from './pages/auth/signIn'
 import { SignUp } from './pages/auth/signUp'
 
@@ -14,6 +15,10 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
       },
     ],
   },
